70b69dae46509bdc7448c6a3ea854dd6f443381c
iamrfowaisqasim <rfowaisqasim@gmail.com>
2023-09-15 13:41:38 +0500

    the basic structure of todo list is complete

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,24 +1,74 @@
 import { useState } from "react"
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import CommentIcon from '@mui/icons-material/Comment';
 
 
 const currentTodos = [
     { id: 1, text: 'Run the cat', completed: false },
     { id: 2, text: 'run the dog', completed: true },
     { id: 3, text: 'run the frog', completed: false },
-    { id: 5, text: 'run the elephant', completed: false },
+    { id: 5, text: 'run the elephant', completed: true },
 ]
 export const TodoList = () => {
     const [todos, setTodos] = useState(currentTodos)
 
     return (
-        <div>
-            <ul>
-                {todos.map(todo => {
-                    return <li key={todo.id}>
-                        {todo.text} {todo.completed ? "✓" : "✗"}
-                    </li>
-                })}
-            </ul>
-        </div>
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+            {todos.map(todo => {
+                const labelId = `checkbox-list-label-${todo.id}`
+                return <ListItem
+                    key={todo.id}
+                    secondaryAction={
+                        <IconButton edge="end" aria-label="comments">
+                            <CommentIcon />
+                        </IconButton>
+                    }
+                    disablePadding
+                >
+                    <ListItemButton role={undefined}
+                        dense
+                    >
+                        <ListItemIcon>
+                            <Checkbox
+                                edge="start"
+                                checked={todo.completed}
+                                tabIndex={-1}
+                                disableRipple
+                                inputProps={{ 'aria-labelledby': labelId }}
+                            />
+                        </ListItemIcon>
+                        <ListItemText id={labelId} primary={todo.text} />
+                    </ListItemButton>
+                </ListItem>
+            })}
+        </List>
     )
 }
+
+
+
+
+// export default function CheckboxList() {
+//   const [checked, setChecked] = useState([0]);
+
+//   const handleToggle = (value) => () => {
+//     const currentIndex = checked.indexOf(value);
+//     const newChecked = [...checked];
+
+//     if (currentIndex === -1) {
+//       newChecked.push(value);
+//     } else {
+//       newChecked.splice(currentIndex, 1);
+//     }
+
+//     setChecked(newChecked);
+//   };
+
+
+// }
